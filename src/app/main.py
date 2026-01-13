7f72229ad4b8995a6aebd5290e8c09aec9674a67
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-01-13 12:06:06 -0300

    reestruturar pastas e inserir comentários

--- src/app/main.py
+++ src/app/main.py
@@ -3,19 +3,29 @@ import traceback
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+# instanciar app Flask
 app = Flask(__name__)
+# configurar CORS
 CORS(app)
 
+# criar rota para importar letras
 @app.route('/import', methods=['POST'])
 def importar():
+
+  # obter dados JSON da requisição
   data = request.json
   
   try:
+    # extrair URL da música
     url = data.get("url")
+
     return jsonify({ "status": "ok" })
   except Exception as e:
+
+    # imprimir rastreamento de pilha para depuração
     traceback.print_exc()
     return jsonify({ "status": "error", "message": str(e) }), 500
   
+# iniciar app Flask
 if (__name__ == '__main__'):
   app.run(port=3000, debug=True)
